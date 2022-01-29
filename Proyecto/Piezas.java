60201f02066f80d81e13321e54f8dfcbe2cc8ef1
JavierMQ28 <javier_manceraq@ciencias.unam.mx>
2022-01-28 23:43:08 -0600

    Agregando la clase Piezas

---
+++ Proyecto/Piezas.java
@@ -0,0 +1,26 @@
+public class Piezas{
+
+    protected int x;
+    protected int y;
+    
+    public Piezas (int x, int y){
+	this.x = x;
+	this.y = y;
+    }
+    
+    public int getXs(){
+	return x;
+    }
+
+    public void setXs(int x){
+        this.x = x;
+    }
+    
+    public int getYs(){
+	return y;
+    }
+
+    public void setYs(int y){
+        this.y = y;
+    }
+}
